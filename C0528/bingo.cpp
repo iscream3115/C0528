821cac47f23acab6a75a0393420d3f651b66f71f
관희 이 <rumble3115@naver.com>
2023-05-29 16:25:26 +0900

    빙고 완성

--- C0528/bingo.cpp
+++ C0528/bingo.cpp
@@ -1,58 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 
-void shuffle(int arr[][5]);
 
+void shuffleArray(int arr[], int size);
+void printGrid(int grid[][5]);
+int checkBingo(int grid[][5]);
 
 int main()
 {
+    int bingo[5][5];
+    int num[25];
+    int bingoNum = 0;
+    int input = 0;
 
-	int bingo[5][5] = { 0 };
+    for (int i = 0; i < 25; i++) 
+    {
+        num[i] = i + 1;
+    }
 
-	srand((unsigned int)time(NULL));
+    shuffleArray(num, 25);
 
-	shuffle(bingo);
+    // 섞인 숫자를 5x5 그리드에 저장
+    int index = 0;
+    for (int i = 0; i < 5; i++) 
+    {
+        for (int j = 0; j < 5; j++) 
+        {
+            bingo[i][j] = num[index];
+            index++;
+        }
+    }
 
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			printf("%d		",bingo[i][j]);
-		}
-		printf("\n");
-	}
+    printGrid(bingo);
 
+    while (bingoNum < 3)
+    {
+        printf("원하는 숫자를 입력(1~25까지): ");
+         scanf("%d",&input);
 
+         for (int i = 0; i < 5; i++)
+         {
+             for (int j = 0; j < 5; j++)
+             {
+                 if (bingo[i][j] == input)
+                 {
+                     bingo[i][j] = 'X';
+                     break;
 
+                 }
+             }
+         }
 
+         printGrid(bingo);
 
-	return 0;
+         bingoNum = checkBingo(bingo);
+
+         printf("빙고 갯수: %d\n", bingoNum);
+    }
+
+    
+
+    return 0;
 }
 
-void shuffle(int arr[][5])
+void shuffleArray(int arr[], int size) 
 {
-	int src = 0, dest = 0;
-	int num = 1;
-	int count = 0;
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			arr[i][j] = num++;
-		}
+    srand((unsigned int)time(NULL));
 
-	}
+    for (int i = size - 1; i > 0; i--) 
+    {
+        int j = rand() % (i + 1);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
 
-	while (count < 12)
-	{
-		arr[rand() % 5][rand() % 5] = arr[rand() % 5][rand() % 5];
-		count++;
-	}
+void printGrid(int grid[][5]) 
+{
+    for (int i = 0; i < 5; i++) 
+    {
+        for (int j = 0; j < 5; j++) 
+        {
+            if (grid[i][j] > 25) printf("X\t");
+            else printf("%d\t", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int checkBingo(int grid[][5])
+{
+    int count = 0;
+    int totalBingo = 0;
+
+    for (int i = 0; i < 5 ; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (grid[i][j] > 80) count++;
+        }
+        if (count == 5) totalBingo++;
+        count = 0;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (grid[j][i] > 80) count++;
+        }
+        if (count == 5) totalBingo++;
+        count = 0;
+    }
 
 
 
+    return totalBingo;
 
 
 }
